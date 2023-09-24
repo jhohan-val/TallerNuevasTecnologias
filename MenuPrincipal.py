6baaefc2ce1f7499a930c28549e29cbf19702f09
jhohan-val <valderramajhohan@gmail.com>
2023-09-24 01:32:28 -0500

    Se crea el buscador por id de las agrupaciones

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -8,15 +8,16 @@ os.system("cls")
 opcion=0
 
 
-while (opcion !=5):
+while (opcion !=6):
     Participante={}
     print ("Bienvenido a", nombreFestival,"musical Festival \n")
     print ("************************************\n")
     print ("1) Registrar una agrupacion musical o DJ")
-    print ("2) Listado de los participantes")
-    print ("3) Modificar la hora de presentacion")
-    print ("4) Retirar una agrupacion musical o DJ")
-    print ("5) Salir\n")
+    print ("2) Listado de los participantes que no se han presentado")
+    print ("3) Cambiar el estado de un participante")
+    print ("4) Modificar la hora de presentacion")
+    print ("5) Retirar una agrupacion musical o DJ")
+    print ("6) Salir\n")
     opcion = int(input("Digita una opcion: "))
 
     if opcion==1:
@@ -26,23 +27,44 @@ while (opcion !=5):
         Participante["genero"] = input("Digita el genero musical de la agrupacion o DJ: ")
         Participante["hora"] = input("Digita la hora de la presentacion: ")
         Participante["pago"] = float(input("Digita el pago realizado a la agrupacion o DJ: "))
-        Participante["estado"] = input("La agrupacion o DJ ya se presento? si o no: ")
+        Participante["estado"] = False
         Participantes.append(Participante)
         print("\nRegistro exitoso\n")
         os.system("pause")
         os.system("cls")
     elif opcion==2:
         os.system("cls")
-        print("\n", Participantes, "\n")
+        buscarParticipante = int(input("Digita el id de la agrupación o DJ que deseas buscar: "))
+        encontrado = False
+        for participanteEncontrado in Participantes:
+            if participanteEncontrado['id'] == buscarParticipante:
+                encontrado = True
+                estado = "Presentada" if participanteEncontrado["estado"] else "No Presentada"
+                if participanteEncontrado["estado"]:
+                    print(f"La banda {participanteEncontrado['nombre']} ya se ha presentado.")
+                else:
+                    print("\n************************************")
+                    print("\nAgrupación encontrada: \n")
+                    print(f"id: {participanteEncontrado['id']}")
+                    print(f"nombre: {participanteEncontrado['nombre']}")
+                    print(f"genero: {participanteEncontrado['genero']}")
+                    print(f"hora: {participanteEncontrado['hora']}")
+                    print(f"pago: {participanteEncontrado['pago']}")
+                    print(f"estado: {estado}\n")
+        if not encontrado:
+            print(f"No se encontró una banda con el ID {buscarParticipante}")    
         os.system("pause")
         os.system("cls")
     elif opcion==3:
-        print("\nEstas en la opcion 3")
+        os.system("cls")
         os.system("cls")
     elif opcion==4:
+        print("\nEstas en la opcion 3")
+        os.system("cls")
+    elif opcion==5:
         print("\nEstas en la opcion 4")
         os.system("cls")
-    elif opcion>5:
+    elif opcion>6:
         os.system("cls")
         print("\nopcion invalida\n")
         os.system("pause")
